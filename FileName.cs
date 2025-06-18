4d0f5ba37d5626be4d277e4b5e2275a656c86fd8
goldihell <yuval.gold21@gmail.com>
2025-06-18 16:28:37 +0300

    part 2 of calculator to Nick 😧 but this time exactly how Nick wanted
    Now the calculator can work according to the order of operations of arithmetic.
    And all thanks to using System.Data

--- FileName.cs
+++ FileName.cs
@@ -3,152 +3,96 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data; 
 
 namespace Calculator_for_nick
 {
     internal class Program
     {
-        enum CalculatorActions { None, Add, Subtract, Multiply, Divide }
-
         static void Main(string[] args)
         {
             Console.WriteLine("Enter numbers or action you want please (+, -, *, /,=)");
 
-            float currentResult = 0;
-            CalculatorActions lastAction = CalculatorActions.None;
-            bool expectingNumber = true;
-
             List<string> expressionParts = new List<string>();
+            bool expectingNumber = true;
 
             while (true)
             {
                 if (expectingNumber)
                 {
-                    Console.Write("Current result:" + currentResult+". Enter a number: ");
+                    Console.Write("Current expression: " + string.Join(" ", expressionParts) + ". Enter a number: ");
                 }
                 else
                 {
-                    Console.Write("Current result:"+ currentResult+ ". Enter an action (+, -, *, /,=):");
+                    Console.Write("Current expression: " + string.Join(" ", expressionParts) + ". Enter an action (+, -, *, /,=):");
                 }
 
                 string input = Console.ReadLine().ToLower().Trim();
 
-
                 if (input == "=")
                 {
-                    if (expectingNumber && lastAction != CalculatorActions.None)
-                    {
-                        Console.WriteLine("Error you can't end with action. enter number");
-                        continue;
-                    }
-                    else if (!expressionParts.Any())
+                    if (expressionParts.Count == 0)
                     {
                         Console.WriteLine("Please enter number before you enter =");
                         continue;
                     }
 
-                    Console.WriteLine(string.Join("", expressionParts)+"="+currentResult);
 
-                    currentResult = 0;
-                    lastAction = CalculatorActions.None;
+
+                    string fullExpression = string.Join("", expressionParts);
+
+                    try
+                    {
+                        DataTable dt = new DataTable();
+                        var result = dt.Compute(fullExpression, string.Empty);
+
+                        Console.WriteLine(fullExpression+"="+result);
+
+                        expressionParts.Clear();
+                        expectingNumber = true;
+                        continue;
+                    }
+                    catch (SyntaxErrorException ex)
+                    {
+                        Console.WriteLine("Error in expression: "+ex.Message+". Please check your input.");
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        Console.WriteLine("Error!!! you can't divide by zero!");
+                    }
+                    expressionParts.Clear();
                     expectingNumber = true;
-                    expressionParts.Clear(); ;
+                    Console.WriteLine("Starting a new calculation.");
                     continue;
                 }
 
+                float numberInput;
+                bool isNumber = float.TryParse(input, out numberInput);
+
                 if (expectingNumber)
                 {
-                    bool isNumber = float.TryParse(input, out float numberInput);
                     if (isNumber)
                     {
-                        if (lastAction == CalculatorActions.None)
-                        {
-                            currentResult = numberInput;
-                        }
-                        else
-                        {
-                            currentResult = CalculateResult(lastAction, currentResult, numberInput);
-                        }
-                        expressionParts.Add(input); 
-                        expectingNumber = false; 
-                        lastAction = CalculatorActions.None; 
+                        expressionParts.Add(input);
+                        expectingNumber = false;
                     }
                     else
                     {
                         Console.WriteLine("It is not relevant");
                     }
                 }
-                else 
+                else
                 {
-                    CalculatorActions newAction = GetActionFromString(input);
-                    if (newAction != CalculatorActions.None)
+                    if (input == "+" || input == "-" || input == "*" || input == "/")
                     {
-                        lastAction = newAction; 
-                        expressionParts.Add(GetActionSymbol(newAction)); 
-                        expectingNumber = true; 
+                        expressionParts.Add(input);
+                        expectingNumber = true;
                     }
                     else
                     {
                         Console.WriteLine("Error!!! Please enter action (+, -, *, /,=) ");
                     }
                 }
-            }
-        }
-
-        static CalculatorActions GetActionFromString(string input)
-        {
-            switch (input)
-            {
-                case "+":
-                    return CalculatorActions.Add;
-                case "-":
-                    return CalculatorActions.Subtract;
-                case "*":
-                    return CalculatorActions.Multiply;
-                case "/":
-                    return CalculatorActions.Divide;
-                default:
-                    return CalculatorActions.None; 
-            }
-        }
-
-        static string GetActionSymbol(CalculatorActions action)
-        {
-            switch (action)
-            {
-                case CalculatorActions.Add:
-                    return "+";
-                case CalculatorActions.Subtract:
-                    return "-";
-                case CalculatorActions.Multiply:
-                    return "*";
-                case CalculatorActions.Divide:
-                    return "/";
-                default:
-                    return ""; 
-            }
-        }
-
-        static float CalculateResult(CalculatorActions action, float num1, float num2)
-        {
-            switch (action)
-            {
-                case CalculatorActions.Add:
-                    return num1 + num2;
-                case CalculatorActions.Subtract:
-                    return num1 - num2;
-                case CalculatorActions.Multiply:
-                    return num1 * num2;
-                case CalculatorActions.Divide:
-                    if (num2 == 0)
-                    {
-                        Console.WriteLine("Error!!! you can't divide by zero!");
-                        return 0;
-                    }
-                    return num1 / num2;
-                default:
-                    Console.WriteLine("Error!!! Unknown action for calculation.");
-                    return num1;
             }
         }
     }
